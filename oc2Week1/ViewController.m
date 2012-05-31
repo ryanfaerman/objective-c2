89d9d0b87ea833b35e30ced753a4006099c04fb2
Ryan Faerman <ryan@faerman.net>
2012-05-30 23:14:58 -0400

    it all works

--- oc2Week1/ViewController.m
+++ oc2Week1/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ShapeFactory.h"
 
 @interface ViewController ()
 
@@ -16,8 +17,30 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+  [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  
+  // Build some shapes, like a boss!
+  ShapeClass *square = [ShapeFactory CreateShape:0];
+  ShapeClass *triangle = [ShapeFactory CreateShape:1];
+  ShapeClass *rectangle = [ShapeFactory CreateShape:2];
+  
+  
+  // Add the square label, like a boss!
+  UILabel *squareLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 10.0f, 320.0f, 30.0f)];
+  squareLabel.text = [[NSString alloc] initWithFormat:@"Shape Square Area: %d", [square GetArea]];
+  [self.view addSubview:squareLabel];
+  
+  // Add the triangle label, like a boss!
+  UILabel *triangleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 50.0f, 320.0f, 30.0f)];
+  triangleLabel.text = [[NSString alloc] initWithFormat:@"Shape Triangle Area: %d", [triangle GetArea]];
+  [self.view addSubview:triangleLabel];
+  
+  // Add the rectangle label, like a boss!
+  UILabel *rectangleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 90.0f, 320.0f, 30.0f)];
+  rectangleLabel.text = [[NSString alloc] initWithFormat:@"Shape Rectangle Area: %d", [rectangle GetArea]];
+  [self.view addSubview:rectangleLabel];
+  
 }
 
 - (void)viewDidUnload
