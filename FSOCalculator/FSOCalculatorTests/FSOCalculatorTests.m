a4aa05c47b9cb7f788fd56abb47488ecc56000c2
Ryan Faerman <ryan@faerman.net>
2012-06-06 22:51:55 -0400

    started week 2 project

---
+++ FSOCalculator/FSOCalculatorTests/FSOCalculatorTests.m
@@ -0,0 +1,32 @@
+//
+//  FSOCalculatorTests.m
+//  FSOCalculatorTests
+//
+//  Created by Ryan Faerman on 6/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FSOCalculatorTests.h"
+
+@implementation FSOCalculatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FSOCalculatorTests");
+}
+
+@end
