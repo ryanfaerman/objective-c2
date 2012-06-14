4f6d0cb70ea9e909cd1d90114e7c6daec1815dc2
Ryan Faerman <ryan@faerman.net>
2012-06-13 22:15:41 -0400

    week3 project data

---
+++ FSOEvents/FSOEvents/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  FSOEvents
+//
+//  Created by Ryan Faerman on 6/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
