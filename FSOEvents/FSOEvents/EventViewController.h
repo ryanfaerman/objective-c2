1f0ee7a9bbd4d45de395db6c846b4b9c5766885c
Ryan Faerman <ryan@faerman.net>
2012-06-13 22:48:25 -0400

    prepping second view

---
+++ FSOEvents/FSOEvents/EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventViewController.h
+//  FSOEvents
+//
+//  Created by Ryan Faerman on 6/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UIViewController
+
+@end
